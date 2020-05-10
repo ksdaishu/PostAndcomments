b02cf0bcf70097d13016fd3af852b991789e222a
ksdaishu <ksdaishu@gmail.com>
2020-05-10 22:31:05 +0530

    Navbar added

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,15 @@
 module ApplicationHelper
 	def login_helper
-          link_to "logout", destroy_user_session_path , method: :delete      		
+        link_to "logout", destroy_user_session_path , method: :delete     		
 	end
+
+	def full_title(page_title = '')
+  		base_title = 'Post and Command'
+  		if page_title.empty?
+    		base_title
+  		else
+    		page_title + " | " + base_title
+  		end
+	end
+
 end
