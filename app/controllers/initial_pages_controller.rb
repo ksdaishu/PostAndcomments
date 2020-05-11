fe75731914da9b8f3f66e6e7159d0bc75f318a8c
ksdaishu <ksdaishu@gmail.com>
2020-05-11 09:25:49 +0530

    User page redirection isssue fix

--- app/controllers/initial_pages_controller.rb
+++ app/controllers/initial_pages_controller.rb
@@ -1,7 +1,8 @@
 class InitialPagesController < ApplicationController
   def home
-  	if user_signed_in?
-          
+    if user_signed_in?
+      @user = current_user
+      render 'users/show'      
     end
   end
 end
